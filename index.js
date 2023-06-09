282d130495882a903f4c9bc38c98540fb2713f41
Armans0701~ <sikder89098@gmail.com>
2023-06-09 16:16:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require ('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+// Middlewere
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('school is running')
+})
+
+app.listen(port,()=>{
+    console.log(`Summer champ is running on port ${port}`);
+})
