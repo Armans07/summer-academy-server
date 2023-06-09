c63021122537773221c69cd2944d027b74aaa439
Armans0701~ <sikder89098@gmail.com>
2023-06-09 22:48:04 +0600

    added selected collections

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require ('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // Middlewere
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxlectq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,23 +26,46 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const classCollection = client.db("summerDB").collection("classes");
+    const instructorsCollection = client
+      .db("summerDB")
+      .collection("instructors");
+    const selectedCollection = client.db("summerDB").collection("selected");
 
-    const classCollection = client.db("summerDB").collection("classes")
-    const instructorsCollection = client.db("summerDB").collection("instructors")
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/classes',async (req,res)=>{
-        const result = await classCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/instructors',async (req,res)=>{
-        const result = await classCollection.find().toArray()
-        res.send(result)
-    })
+    // Selected collection
+
+    app.get("/selected", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await selectedCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/selected", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await selectedCollection.insertOne(item);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,13 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("school is running");
+});
 
-
-
-app.get('/',(req,res)=>{
-    res.send('school is running')
-})
-
-app.listen(port,()=>{
-    console.log(`Summer champ is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Summer champ is running on port ${port}`);
+});
