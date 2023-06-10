68ca2e219013d686d4b9a8bcad3663610f41dbd2
Armans0701~ <sikder89098@gmail.com>
2023-06-10 14:48:37 +0600

    added add class

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // Middlewere
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxlectq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +32,13 @@ async function run() {
       .db("summerDB")
       .collection("instructors");
     const selectedCollection = client.db("summerDB").collection("selected");
+    const usersCollection = client.db("summerDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get("/classes", async (req, res) => {
       const result = await classCollection.find().toArray();
@@ -42,7 +50,26 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/addclass", async (req, res) => {
+      const body = req.body;
+      body.createdAt = new Date();
+
+      const result = await instructorsCollection.insertOne(body);
+      console.log(result);
+      if (!body) {
+        return res.status(404).send({ message: "body data not found" });
+      }
+      res.send(result);
+    });
+
     // Selected collection
+
+    app.delete("/selected/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get("/selected", async (req, res) => {
       const email = req.query.email;
@@ -61,11 +88,18 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/selected/id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await selectedCollection.deleteOne(query);
-      res.send(result);
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
